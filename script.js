5e4c7ed39aafa8552dff7a28d0a2a09626dcdff5
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-13 16:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const parseCode = (str) => {
-  // your code here
+  // Split the input string using the "000" separator
+  const parts = str.split("000");
+
+  // Extract the first name, last name, and id from the parts array
+  const firstName = parts[0];
+  const lastName = parts[1];
+  const id = parts[2];
+
+  // Return an object with the extracted properties
+  return { firstName, lastName, id };
 };
 
-// Do not change the code below
+// Example usage:
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));

45d22ee159f87c363883f7c923adc7c2c3b59fc5
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-13 16:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const parseCode = (str) => {
   // Split the input string using the "000" separator
   const parts = str.split("000");
 
+  // Ensure that there are exactly three parts
+  if (parts.length !== 3) {
+    return { error: "Invalid input format" };
+  }
+
   // Extract the first name, last name, and id from the parts array
   const firstName = parts[0];
   const lastName = parts[1];
